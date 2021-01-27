1121a0953d18ca3f32544d8c99bd57f10c48c9d9
sanimridha <sanimridha307@gmail.com>
2021-01-27 12:41:06 +0600

    adding video url with some properties

--- screens/VideoPlayerScreen.tsx
+++ screens/VideoPlayerScreen.tsx
@@ -2,11 +2,22 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 import { Text, View } from '../components/Themed';
+import {Video} from 'expo-av'
 
 export default function VideoPlayerScreen() {
   return (
     <View style={styles.container}>
-
+      <Video
+        source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+        rate={1.0}
+        volume={1.0}
+        isMuted={false}
+        resizeMode="cover"
+        shouldPlay ={false}
+        isLooping = {false}
+        useNativeControls
+        style={{ width: 300, height: 300 }}
+/>
     </View>
   );
 }
