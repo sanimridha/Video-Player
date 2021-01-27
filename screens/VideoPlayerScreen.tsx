62c21a9fceaa06832949f3fbc28ae4d72ad397c5
sanimridha <sanimridha307@gmail.com>
2021-01-27 15:00:13 +0600

    adding style into for player

--- screens/VideoPlayerScreen.tsx
+++ screens/VideoPlayerScreen.tsx
@@ -1,23 +1,26 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { Dimensions } from "react-native";
+import { Text, View } from "../components/Themed";
+import { Video } from "expo-av";
 
-import { Text, View } from '../components/Themed';
-import {Video} from 'expo-av'
-
+const { width, height } = Dimensions.get("window");
 export default function VideoPlayerScreen() {
   return (
     <View style={styles.container}>
       <Video
-        source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+        source={{
+          uri: "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+        }}
         rate={1.0}
         volume={1.0}
         isMuted={false}
         resizeMode="cover"
-        shouldPlay ={false}
-        isLooping = {false}
+        shouldPlay={false}
+        isLooping={false}
         useNativeControls
-        style={{ width: 300, height: 300 }}
-/>
+        style={styles.Video}
+      />
     </View>
   );
 }
@@ -25,7 +28,11 @@ export default function VideoPlayerScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Video: {
+    width: width,
+    height: height / 3,
   },
 });
